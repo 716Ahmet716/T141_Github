28e011667b3c02993dac9bd646657af2823e109c
aziz eksik <ahmetfea6301@gmail.com>
2024-04-29 15:09:34 +0300

    merhaba

--- src/Ahmet.java
+++ src/Ahmet.java
@@ -2,6 +2,9 @@ public class Ahmet {
     public static void main(String[] args) {
 
         System.out.println("ahmet");
+        System.out.println("efe");
+
+        System.out.println("ahmet716");
 
     }
 }
