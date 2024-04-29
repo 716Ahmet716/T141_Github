ed4c0713d182bc14c3056ab97a0c2bbe25c3e377
ahmet efe <efea6301@gmail.com>
2024-04-29 16:00:07 +0300

    second version

---
+++ src/P02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P02 {
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+
+        System.out.println("Adinizi giriniz");
+        String isim= scn.nextLine();
+
+
+
+
+    }
+}
